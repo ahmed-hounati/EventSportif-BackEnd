9a5418ee2459acf76dc5cefb8cb028baddfc7282
ahmed-hounati <hounatiahmed@gmail.com>
2024-11-25 10:19:59 +0100

    Feat: Implement the connection and initialize the first organizer

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const Organizer = require('./models/UserModel')
 require('dotenv').config();
-
-
 const app = express();
 app.use(express.json());
-app.use(cors());
-
-
 
 
 mongoose.connect(process.env.MONGO_URI, {
 }).then(() => {
     console.log('Connected to MongoDB');
-    initializeAdmin();
+    initializeOrganizer();
 })
     .catch((err) => console.error('Error connecting to MongoDB:', err));
 
-app.use('/api', userRoutes);
 
 
-
-async function initializeAdmin() {
+async function initializeOrganizer() {
     try {
-        const admin = await Admin.findOne({ email: 'admin@gmail.com' });
-        if (!admin) {
-            const hashedPassword = await bcrypt.hash('123456789', 8);
+        const organizer = await Organizer.findOne({ email: process.env.EMAIL });
+        if (!organizer) {
+            const hashedPassword = await bcrypt.hash(process.env.PASSWORD, 8);
 
-            const newAdmin = new Admin({
-                name: 'admin',
-                email: 'admin@gmail.com',
+            const newOrganizer = new Organizer({
+                name: process.env.NAME,
+                email: process.env.EMAIL,
                 password: hashedPassword,
-                status: 'active',
-                role: 'admin'
+                role: 'Organizer'
             });
-            await newAdmin.save();
-            console.log('Admin user created with email: admin@gmail.com');
+            await newOrganizer.save();
+            console.log('Organizer created successfully');
         } else {
         }
     } catch (error) {
-        console.error('Error initializing admin:', error);
+        console.error('Error initializing Organizer:', error);
     }
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
