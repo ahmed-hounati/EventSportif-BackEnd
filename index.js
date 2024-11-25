b62033a3899f7f1e2417188a9fa692d4dff1a029
ahmed-hounati <hounatiahmed@gmail.com>
2024-11-25 09:37:41 +0100

    fc

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+require('dotenv').config();
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+
+mongoose.connect(process.env.MONGO_URI, {
+}).then(() => {
+    console.log('Connected to MongoDB');
+    initializeAdmin();
+})
+    .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+app.use('/api', userRoutes);
+
+
+
+async function initializeAdmin() {
+    try {
+        const admin = await Admin.findOne({ email: 'admin@gmail.com' });
+        if (!admin) {
+            const hashedPassword = await bcrypt.hash('123456789', 8);
+
+            const newAdmin = new Admin({
+                name: 'admin',
+                email: 'admin@gmail.com',
+                password: hashedPassword,
+                status: 'active',
+                role: 'admin'
+            });
+            await newAdmin.save();
+            console.log('Admin user created with email: admin@gmail.com');
+        } else {
+        }
+    } catch (error) {
+        console.error('Error initializing admin:', error);
+    }
+}
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
