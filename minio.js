2e903d40686e728ed6b4143238ab6248423535c3
ahmed-hounati <hounatiahmed@gmail.com>
2024-11-29 23:31:32 +0100

    fix some errors

--- minio.js
+++ minio.js
@@ -2,7 +2,7 @@ const Minio = require('minio');
 require('dotenv').config();
 
 const minioClient = new Minio.Client({
-    endPoint: '127.0.0.1',
+    endPoint: 'minio',
     port: 9000,
     useSSL: false,
     accessKey: process.env.MINIO_ACCESS_KEY,
